4b13148c8682c20c3b5d956c6d7a26a492ba1898
Timur <36734601+TSecretT@users.noreply.github.com>
2021-08-28 17:26:57 +0600

    added demo gif

--- functions/utils.js
+++
@@ -1,8 +0,0 @@
-// Convert snapshot to list
-const getListFromSnapshot = (snapshot) => {
-    let items = [];
-    snapshot.forEach(item => { items.push(item.val()) })
-    return items;
-}
-//
-module.exports = { getListFromSnapshot }
