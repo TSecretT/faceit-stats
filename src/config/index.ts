39568a72f0e4b59ea0a5e11111c8ed2ea5c0ab1c
Timur <36734601+TSecretT@users.noreply.github.com>
2022-03-30 08:56:03 +0200

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,10 @@
+const DEV: boolean = window.location.host === "faceit.tips"? false : true;
+
 const config: any = {
+    DEV,
     VERSION: "0.0.3",
     WELCOME_MESSAGE: "Welcome to Faceit Tips! This app is currently in Beta version, meaning some features might not work as they should, or they might be added and removed in the future. Enjoy!",
-    BASE_URL: "http://localhost:8000"
+    BASE_URL: DEV ? "http://localhost:8000" : "https://faceit.tips"
 }
 
 export default config;
